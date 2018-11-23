9be800bf9d77afd95cfc03017e9679815079e35a
Andrei Ivanov <v-andiva@microsoft.com>
2018-11-23 15:55:02 +0300

    Добавлен функционал настроек

--- StopHrap/Resources/Model/SettingsModel.cs
+++ StopHrap/Resources/Model/SettingsModel.cs
@@ -9,11 +9,13 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SQLite;
 
 namespace StopHrap.Resources.Model
 {
     public class SettingsModel
     {
+        [PrimaryKey]
         public int Id { get; set; }
         public decimal CompareCoef { get; set; }
         public decimal OpenThreshold { get; set; }
